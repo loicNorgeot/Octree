f658155e6e2ce1ee80b4fd2763d5f965f6d4e1fc
Loic Norgeot <loic.norgeot@gmail.com>
2017-07-05 19:25:24 +0200

    Fixes the neighbour tetrahedras bug, adds debugging .mesh

--- computeStreamLines.py
+++ computeStreamLines.py
@@ -14,18 +14,21 @@ def fun(f, q_in, q_out):
             break
         q_out.put((i, f(x)))
 def parmap(f, X, nprocs=multiprocessing.cpu_count()):
-    q_in = multiprocessing.Queue(1)
-    q_out = multiprocessing.Queue()
-    proc = [multiprocessing.Process(target=fun, args=(f, q_in, q_out))
-            for _ in range(nprocs)]
-    for p in proc:
-        p.daemon = True
-        p.start()
-    sent = [q_in.put((i, x)) for i, x in enumerate(X)]
-    [q_in.put((None, None)) for _ in range(nprocs)]
-    res = [q_out.get() for _ in range(len(sent))]
-    [p.join() for p in proc]
-    return [x for i, x in sorted(res)]
+    if nprocs>1:
+        q_in = multiprocessing.Queue(1)
+        q_out = multiprocessing.Queue()
+        proc = [multiprocessing.Process(target=fun, args=(f, q_in, q_out))
+                for _ in range(nprocs)]
+        for p in proc:
+            p.daemon = True
+            p.start()
+        sent = [q_in.put((i, x)) for i, x in enumerate(X)]
+        [q_in.put((None, None)) for _ in range(nprocs)]
+        res = [q_out.get() for _ in range(len(sent))]
+        [p.join() for p in proc]
+        return [x for i, x in sorted(res)]
+    else:
+        return [f(x) for x in X]
 
 #Maths
 def isPointInTetra(pt,pts):
@@ -84,10 +87,12 @@ class Octree:
         tmpPts/=self.mesh.dims
         tmpPts*=2**self.depth
         tmpPts[tmpPts==2**self.depth]=2**self.depth-1
-        self.indices = tmpPts.astype(np.int16, copy=False)
+        self.ptsIndices = tmpPts.astype(np.int16, copy=False)
     def distributeTetras(self):
-        for indTetra, tetra in enumerate(self.mesh.tets):
-            ptInds = self.indices[tetra[:4]]
+        done = np.zeros((len(self.mesh.verts)),dtype="bool_")
+        self.tetsIndices = []
+        for i,tet in enumerate(self.mesh.tets):
+            ptInds = self.ptsIndices[tet[:4]]
             mi, ma = np.min(ptInds,axis=0), np.max(ptInds,axis=0)
             diff = ma-mi
             nb = np.sum(diff)
@@ -96,12 +101,13 @@ class Octree:
                     ptInds=mi+cartesian([range(diff[0]+1),range(diff[1]+1),range(diff[2]+1)])
                 else:
                     ptInds=mi+cartesian([[1,0],[1,0],[1,0]])
+            self.tetsIndices.append(ptInds)
         self.tetras = [[[[] for k in range(2**self.depth)] for j in range(2**self.depth)] for j in range(2**self.depth)]
-        for indTetra, tetra in enumerate(self.mesh.tets):
-            i=-1
-            for ptIndex in tetra[:4]:
-                j = self.indices[ptIndex]
-                self.tetras[j[0]][j[1]][j[2]].append(indTetra)
+        for i, tet in enumerate(self.mesh.tets):
+            for ptIndex in tet[:4]:
+                inds = self.tetsIndices[i]
+                for x in inds:
+                    self.tetras[x[0]][x[1]][x[2]].append(i)
     def makeTetraSet(self):
         for i in range(2**self.depth):
             for j in range(2**self.depth):
@@ -125,39 +131,58 @@ class Octree:
             if isPointInTetra(pt, pts):
                 velocity = interpolate(pt,pts,self.mesh.vectors[ptInds])
                 break
+        if velocity is None:
+            if pt[0]<self.mesh.xmin or pt[0]>self.mesh.xmax or pt[1]<self.mesh.ymin or pt[1]>self.mesh.ymax or pt[2]<self.mesh.zmin or pt[2]>self.mesh.zmax:
+                return None
+            else:
+                print "Error: velocity is None at point",pt
+                print "writing debug.mesh"
+                self.saveSubdivision(ptIndex, "debug.mesh", point=pt, wholeDomain=True)
         return velocity
     def computeTrajectory(self, pt, step, maxIt):
         pos = []
-        pos.append(pt)
         velocity = self.computeVelocity(pt)
-        if velocity is None:
-            print "First velocity is none at point",pt
-            sys.exit()
+        pos.append(pt)
         for it in range(maxIt):
             pt = pt + step*velocity
             pos.append(pt)
             velocity = self.computeVelocity(pt)
             if velocity is None:
-                print "Velocity is none at point",pt
                 break
-            if it==maxIt-1:
-                print "Reached end of streamline at point",pt
         return pos
-    def saveSubdivision(self, inds, outfile):
-        mesh2 = msh.Mesh(self.mesh.path)
-        mesh2.readSol()
+    def saveSubdivision(self, inds, outfile, point=None, wholeDomain=None):
+        #Retaining the tetra of the considered subdivision
+        tetraMesh = msh.Mesh(self.mesh.path)
+        tetraMesh.readSol()
         for t in self.tetras[inds[0]][inds[1]][inds[2]]:
-            mesh2.tets[t,-1]=10
+            tetraMesh.tets[t,-1]=10
         for ind in range(10):
-            mesh2.removeRef(ind)
-        mesh2.tris = np.array([])
-        mesh2.discardUnused()
+            tetraMesh.removeRef(ind)
+        tetraMesh.tris = np.array([])
+        tetraMesh.discardUnused()
+
+        #Creating a cube of the subdivision boundaries
         div = self.mesh.dims/8
         mins = [self.mesh.xmin + inds[0]*div[0], self.mesh.ymin + inds[1]*div[1], self.mesh.zmin + inds[2]*div[2]]
-        mesh3=msh.Mesh(cube=[mins[0], mins[0]+div[0], mins[1], mins[1]+div[1], mins[2], mins[2]+div[2]])
-        mesh2.fondre(mesh3)
-        mesh2.write(outfile)
-        mesh2.writeSol(outfile[:-5]+".sol")
+        boundaryMesh=msh.Mesh(cube=[mins[0], mins[0]+div[0], mins[1], mins[1]+div[1], mins[2], mins[2]+div[2]])
+        boundaryMesh.replaceRef(0,2)
+        tetraMesh.fondre(boundaryMesh)
+
+        #Creating a cube around the point
+        if point is not None:
+            off = 0.01
+            point = msh.Mesh(cube=[point[0]-off, point[0]+off, point[1]-off, point[1]+off, point[2]-off, point[2]+off])
+            point.replaceRef(0,1)
+            tetraMesh.fondre(point)
+
+        #Creating a box of the whole domain
+        if wholeDomain is not None:
+            domain = msh.Mesh(cube=[self.mesh.xmin, self.mesh.xmax, self.mesh.ymin, self.mesh.ymax, self.mesh.zmin, self.mesh.zmax])
+            tetraMesh.fondre(domain)
+
+        #Writing the file
+        tetraMesh.write(outfile)
+        tetraMesh.writeSol(outfile[:-5]+".sol")
 
 if __name__ == "__main__":
     print "1 - Opening the .mesh file and the corresponding .sol file"
@@ -175,13 +200,13 @@ if __name__ == "__main__":
     t = time.time()
     #Parameters
     nPoints = 10
-    maxIt   = 1000
-    step    = 0.01
+    maxIt   = 100
+    step    = 0.02
     #First points
     initialXZ = np.random.normal(loc=0.5,scale=0.15,size=(nPoints,2))
     initialPts = np.insert(initialXZ, 1, 0.02, axis=1)-0.5
     initialPts = initialPts*octree.mesh.dims + octree.mesh.center
     #Actual computing
-    positions = parmap(lambda x: octree.computeTrajectory(x, step, maxIt), initialPts)
+    positions = parmap(lambda x: octree.computeTrajectory(x, step, maxIt), initialPts, nprocs=8)
     print "Streamlines computed in", time.time()-t
     print "Length of each streamline:", [len(p) for p in positions]
